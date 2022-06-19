0f06bfb439f0681fca6702eda4768cd679996861
Leon Júnio Martins <juniohuy2gamer@gmail.com>
2022-06-18 21:06:40 -0300

    q07 finalizada e q06,q05 alteradas para 100%

--- tp4/dev/q07/Tp4Q07.java
+++ tp4/dev/q07/Tp4Q07.java
@@ -84,7 +84,7 @@ class Ferramentas {
 	public static boolean gerarLog(double inic, double fim, int comp) {
 		boolean resp = true;
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter(getMatricula() + "_hashRehash.txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(getMatricula() + "_hashIndireta.txt"));
 			bw.write(getMatricula() + "\t" + (fim - inic) / 1000.0 + "\t" + comp);
 			bw.close();
 		} catch (IOException io) {
@@ -430,9 +430,9 @@ class Filme {
 			setDuracao(Integer.parseInt(tratarLinha(line, 3)));
 			line = buff.readLine();
 			boolean chk = false;
-			while (!Ferramentas.myContains(line, "Título original")) {
+			while (!Ferramentas.myContains(line, "T�tulo original")) {
 				line = buff.readLine();
-				if (Ferramentas.myContains(line, "<bdi>Situação</bdi>")) {
+				if (Ferramentas.myContains(line, "<bdi>Situa��o</bdi>")) {
 					setTitulo(getNome());
 					chk = true;
 					break;
@@ -442,7 +442,7 @@ class Filme {
 				setTitulo(tratarLinha(line, 4));
 				line = buff.readLine();
 			}
-			while (!Ferramentas.myContains(line, "Situação")) {
+			while (!Ferramentas.myContains(line, "Situa��o")) {
 				line = buff.readLine();
 			}
 			setSituacao(tratarLinha(line, 5));
@@ -452,7 +452,7 @@ class Filme {
 			}
 			setIdioma(tratarLinha(line, 6));
 			line = buff.readLine();
-			while (!Ferramentas.myContains(line, "Orçamento")) {
+			while (!Ferramentas.myContains(line, "Or�amento")) {
 				line = buff.readLine();
 			}
 			setOrcamento(Float.parseFloat(tratarLinha(line, 7)));
@@ -531,18 +531,18 @@ class Filme {
 				resp = "" + Ferramentas.getMinutos(linha);
 				break;
 			case 4:
-				resp = Ferramentas.myReplace(linha, "<p class=\"wrap\"><strong>Título original</strong> ", "");
+				resp = Ferramentas.myReplace(linha, "<p class=\"wrap\"><strong>T�tulo original</strong> ", "");
 				resp = Ferramentas.myReplace(resp, "</p>", "");
 				break;
 			case 5:
-				resp = Ferramentas.myReplace(linha, "<strong><bdi>Situação</bdi></strong> ", "");
+				resp = Ferramentas.myReplace(linha, "<strong><bdi>Situa��o</bdi></strong> ", "");
 				break;
 			case 6:
 				resp = Ferramentas.myReplace(linha, "<p><strong><bdi>Idioma original</bdi></strong> ", "");
 				resp = Ferramentas.myReplace(resp, "</p>", "");
 				break;
 			case 7:
-				resp = Ferramentas.myReplace(linha, "<p><strong><bdi>Orçamento</bdi></strong> ", "");
+				resp = Ferramentas.myReplace(linha, "<p><strong><bdi>Or�amento</bdi></strong> ", "");
 				resp = Ferramentas.myReplace(resp, "</p>", "");
 				if (Ferramentas.myContains(linha, "-")) {
 					resp = "0.0";
@@ -567,8 +567,8 @@ class Filme {
 
 	private File getFile(String name) throws IOException {
 		File file;
-		//file = new File("/tmp/filmes/" + name);
-		 file = new File("../tmp/filmes/" + name);
+		file = new File("/tmp/filmes/" + name);
+		// file = new File("../tmp/filmes/" + name);
 		if (!file.isFile()) {
 			throw new IOException("O arquivo nao foi encontrado na pasta tmp arquivo:" + name);
 		} else {
@@ -578,19 +578,235 @@ class Filme {
 
 }
 
-class HashList {
+class Lista {
 
-	// Posso usar essa classe para instanciar as filas, pilhas e listas
+	private Celula primeiro, ultimo;
+
+	public Lista() {
+		primeiro = new Celula();
+		ultimo = primeiro;
+	}
+
+	public void inserirFim(Filme obj) {
+		Celula tmp = new Celula(obj);
+		ultimo.prox = tmp;
+		ultimo = ultimo.prox;
+		tmp = null;
+	}
+
+	public void inserirInicio(Filme fm) {
+		Celula tmp = new Celula(fm);
+		tmp.prox = primeiro.prox;
+		primeiro.prox = tmp;
+		if (primeiro == ultimo) {
+			ultimo = tmp;
+		}
+		tmp = null;
+	}
+
+	public void inserir(Filme fm, int pos) throws Exception {
+		int tam = tamanho();
+		if (pos == 0) {
+			inserirInicio(fm);
+		} else if (pos == tam - 1) {
+			inserirFim(fm);
+		} else if (pos < 0 || pos >= tam) {
+			throw new Exception("Posição de inserção invalida - pos:" + pos + " size:" + tam);
+		} else {
+			Celula i = primeiro;
+			for (int j = 0; j < pos; j++, i = i.prox)
+				;
+			Celula tmp = i.prox;
+			i.prox = new Celula(fm);
+			i.prox.prox = tmp;
+			tmp = i = null;
+		}
+	}
+
+	public Filme removerInicio() throws Exception {
+		Filme resp = null;
+		if (primeiro == ultimo) {
+			throw new Exception("A lista esta vazia!");
+		}
+		Celula tmp = primeiro;
+		resp = tmp.prox.elemento;
+		primeiro = tmp.prox;
+		tmp.prox = null;
+		tmp = null;
+		return resp;
+	}
+
+	public Filme removerFim() throws Exception {
+		Filme resp = null;
+		if (primeiro == ultimo) {
+			throw new Exception("A lista esta vazia!");
+		}
+		Celula i;
+		for (i = primeiro; i.prox != ultimo; i = i.prox)
+			;
+		resp = ultimo.elemento;
+		ultimo = i;
+		ultimo.prox = null;
+		i = null;
+		return resp;
+	}
+
+	public Filme remover(int pos) throws Exception {
+		Filme resp = null;
+		int tamanho = tamanho();
+		if (primeiro == ultimo) {
+			throw new Exception("A lista esta vazia!");
+		} else if (pos < 0 || pos >= tamanho) {
+			throw new Exception("Posição de remoção inválida nesse contexto! pos:" + pos + " size:" + tamanho);
+		} else if (pos == 0) {
+			removerInicio();
+		} else if (pos == tamanho() - 1) {
+			removerFim();
+		} else {
+			Celula i = primeiro;
+			for (int j = 0; j < pos; j++, i = i.prox)
+				;
+			Celula tmp = i.prox;
+			resp = tmp.elemento;
+			i.prox = tmp.prox;
+			tmp.prox = null;
+			i = tmp = null;
+		}
+		return resp;
+	}
+
+	public int tamanho() {
+		int resp = 0;
+		for (Celula tmp = primeiro; tmp != ultimo; tmp = tmp.prox) {
+			resp++;
+		}
+		return resp;
+	}
+
+	public Filme localizar(int pos) throws Exception {
+		Filme resp = null;
+		int tam = tamanho();
+		if (primeiro == ultimo) {
+			throw new Exception("A lista se encontra vazia!");
+		} else if (pos == 0) {
+			resp = primeiro.prox.elemento;
+		} else if (pos == tam - 1) {
+			resp = ultimo.elemento;
+		} else if (pos < 0 || pos >= tam) {
+			throw new Exception("Posição para busca inválida na lista!");
+		} else {
+			Celula i = primeiro;
+			for (int j = 0; j < pos; i = i.prox, j++)
+				;
+			resp = i.prox.elemento;
+		}
+		return resp;
+	}
+
+	public boolean localizar(Filme obj) throws Exception {
+		boolean resp = false;
+		if (primeiro == ultimo) {
+			throw new Exception("A lista se encontra vazia!");
+		} else {
+			for (Celula i = primeiro.prox; i != null; i = i.prox) {
+				if (i.elemento.getTitulo().equals(obj.getTitulo())) {
+					resp = true;
+					i = ultimo;
+				}
+			}
+		}
+		return resp;
+	}
+
+	public boolean localizar(String str) throws Exception {
+		boolean resp = false;
+		if (primeiro == ultimo) {
+			throw new Exception("A lista se encontra vazia!");
+		} else {
+			for (Celula i = primeiro.prox; i != null; i = i.prox) {
+				if (i.elemento.getTitulo().equals(str)) {
+					resp = true;
+					i = ultimo;
+				}
+			}
+		}
+		return resp;
+	}
+
+	public void mostrar() throws Exception {
+		if (primeiro == ultimo) {
+			throw new Exception("Lista se encontra vazia!");
+		} else {
+			for (Celula i = primeiro.prox; i != null; i = i.prox) {
+				MyIO.println(i.elemento.toString());
+			}
+		}
+	}
+
+	public void imprimirReverso() {
+		mostrarRec(primeiro.prox);
+	}
+
+	public void imprimir() {
+		int j = 0;
+		for (Celula tmp = primeiro.prox; tmp != null; tmp = tmp.prox, j++) {
+			MyIO.print("[" + j + "] ");
+			tmp.elemento.imprimir();
+		}
+		MyIO.println();
+	}
+
+	public void mostrarRec(Celula i) {
+		if (i != null) {
+			mostrarRec(i.prox);
+			i.elemento.imprimir();
+		}
+	}
+
+	public Filme removerInicioDiff() throws Exception {
+		Filme resp = null;
+		if (primeiro == ultimo) {
+			throw new Exception("A lista se encontra vazia no momento!");
+		} else {
+			Celula tmp = primeiro.prox;
+			resp = tmp.elemento;
+			primeiro.prox = primeiro.prox.prox;
+			tmp.prox = null;
+			tmp = null;
+		}
+		return resp;
+	}
+
+}
+
+class Celula {
+
+	public Celula(Filme obj) {
+		this.elemento = obj;
+	}
+
+	public Celula() {
+		this(null);
+	}
+
+	public Filme elemento;
+	public Celula prox;
+
+}
+
+class HashList {
 
 	public HashList() {
 		this(21);
 	}
 
 	public HashList(int num) {
-		filmes = new Filme[num];
+		filmes = new Lista[num];
+		for (int i = 0; i < num; filmes[i] = new Lista(), i++)
+			;
 	}
 
-	private Filme[] filmes;
+	private Lista[] filmes;
 
 	public void inserir(Filme obj) {
 		int hash = 0;
@@ -598,14 +814,7 @@ class HashList {
 			hash += (int) lt;
 		}
 		hash = hash(obj.getTitulo());
-		if (filmes[hash] == null) {
-			filmes[hash] = obj;
-		} else {
-			hash = reHash(obj.getTitulo());
-			if (filmes[hash] == null) {
-				filmes[hash] = obj;
-			}
-		}
+		filmes[hash].inserirInicio(obj);
 	}
 
 	private int hash(String frase) {
@@ -616,25 +825,6 @@ class HashList {
 		return hash % filmes.length;
 	}
 
-	private int reHash(String frase) {
-		int hash = 1;
-		for (char lt : frase.toCharArray()) {
-			hash += (int) lt;
-		}
-		return hash % filmes.length;
-	}
-
-	public void imprimir() {
-		for (Filme filmeObj : filmes) {
-			filmeObj.imprimir();
-		}
-	}
-
-	/**
-	 * Retorna o timestamp atual
-	 * 
-	 * @return timestamp atual
-	 */
 	public long now() {
 		return new Date().getTime();
 	}
@@ -649,24 +839,22 @@ class HashList {
 		return this.comp;
 	}
 
-	public boolean findSequencial(String nome) {
+	public boolean findSequencial(String nome) throws Exception {
 		boolean resp = false;
 		int comp = 0;
 		int hash = 0;
 		hash = hash(nome);
 		comp++;
-		if (filmes[hash] != null && filmes[hash].getTitulo().equals(nome)) {
-			MyIO.println("Posicao: " + hash);
-			resp = true;
-		} else {
-			// System.out.println("verificando");
-			resp = false;
-			hash = reHash(nome);
-			comp++;
-			if (filmes[hash] != null && filmes[hash].getTitulo().equals(nome)) {
+		try {
+			if (filmes[hash].localizar(nome)) {
 				MyIO.println("Posicao: " + hash);
 				resp = true;
+			} else {
+				// System.out.println("verificando");
+				resp = false;
 			}
+		} catch (Exception e) {
+			resp = false;
 		}
 		setComp(comp);
 		return resp;
@@ -674,50 +862,55 @@ class HashList {
 
 }
 
-public class Tp4Q06 {
+public class Tp4Q07 {
 
 	private static boolean isFim(String entrada) {
 		return entrada.length() == 3 && Ferramentas.myEquals(entrada, "FIM");
 	}
 
 	public static void main(String[] args) {
-		ArrayList<String> entradas = new ArrayList<>();
-		ArrayList<String> verificacoes = new ArrayList<>();
-		HashList lista;
-		double fim, inic;
-		int comp = 0;
-		String entrada = "";
-		MyIO.setCharset("UTF-8");
-		do {
-			entrada = MyIO.readLine();
-			if (!isFim(entrada)) {
-				entradas.add(entrada);
-			}
-		} while (!isFim(entrada));
-		do {
-			entrada = MyIO.readLine();
-			if (!isFim(entrada)) {
-				verificacoes.add(entrada);
-			}
-		} while (!isFim(entrada));
+		try {
+			ArrayList<String> entradas = new ArrayList<>();
+			ArrayList<String> verificacoes = new ArrayList<>();
+			HashList lista;
+			double fim, inic;
+			int comp = 0;
+			String entrada = "";
+			MyIO.setCharset("UTF-8");
+			do {
+				entrada = MyIO.readLine();
+				if (!isFim(entrada)) {
+					entradas.add(entrada);
+				}
+			} while (!isFim(entrada));
+			do {
+				entrada = MyIO.readLine();
+				if (!isFim(entrada)) {
+					verificacoes.add(entrada);
+				}
+			} while (!isFim(entrada));
 
-		lista = new HashList();
-		for (String ent : entradas) {
-			Filme filme = new Filme(ent);
-			lista.inserir(filme);
-		}
-
-		// procurando sequencialmente
-		inic = lista.now();
-		for (String ver : verificacoes) {
-			MyIO.println("=> " + ver);
-			if (!lista.findSequencial(ver)) {
-				MyIO.println("NAO");
+			lista = new HashList();
+			for (String ent : entradas) {
+				Filme filme = new Filme(ent);
+				lista.inserir(filme);
 			}
-			comp += lista.getComparacoes();
+
+			// procurando sequencialmente
+			inic = lista.now();
+			for (String ver : verificacoes) {
+				MyIO.println("=> " + ver);
+				if (!lista.findSequencial(ver)) {
+					MyIO.println("NAO");
+				}
+				comp += lista.getComparacoes();
+			}
+			fim = lista.now();
+			Ferramentas.gerarLog(inic, fim, comp);
+		} catch (Exception e) {
+			MyIO.println("Erro no main --> " + e.getMessage());
+			e.printStackTrace();
 		}
-		fim = lista.now();
-		Ferramentas.gerarLog(inic, fim, comp);
 	}
 
 }
